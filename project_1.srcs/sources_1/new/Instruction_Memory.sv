5f5ceb36264266ab5bc5eaabcbfd843a0f4ce45d
vbnqwe <71627571+vbnqwe@users.noreply.github.com>
2023-05-26 00:40:35 -0700

    start

---
+++ project_1.srcs/sources_1/new/Instruction_Memory.sv
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+
+
+module Instruction_Memory(
+        input [31:0]    input_stream [15:0],
+        input           clk,
+        output [31:0]   output_stream
+    );
+endmodule
