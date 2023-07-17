9a9941e9b438f8e3a5b1639566dcc3d64a9cb43a
vbogdev <Vbogdev2@gmail.com>
2023-07-17 11:01:12 -0700

    added file for rename_top module
    just a quick commit, just a frame added while I plan out rename stage in depth.

--- project_1.srcs/sources_1/new/ARF.sv
+++ project_1.srcs/sources_1/new/ARF.sv
@@ -9,8 +9,14 @@ Destcription:
     
     
 Inputs:
-    -rX/rX_valid: if rX_valid[i] is high, then will read out rX[i]
-    -dest/dest_valid: same as rX/rX_valid, but with additional logic for connecting to ROB, destination of operation
+    -rX: read out register at address rX
+    -dest/dest_valid: dest is logical register destination, dest_valid specifies if tag will need to be set
+    -clk: if you don't know this then there is an issue
+    -physical_dest: physical destination register of an instruction assigned in ROB, equal to tag
+    -no_available: if high, cannot write tags
+    
+Outputs:
+    -rX_out: output of read register
 
 Outputs:
 */
@@ -24,7 +30,8 @@ module ARF(
         input [4:0] r1 [4],
         input [4:0] r2 [4],
         output [31:0] r1_out [4],
-        output [31:0] r2_out [4]
+        output [31:0] r2_out [4],
+        output [3:0] r1_ready, r2_ready
     );
     
     reg [31:0] core [32]; //data storage
@@ -33,7 +40,7 @@ module ARF(
     reg valid [32]; //if data in ARF entry is valid
     
     reg [6:0] intermediate_tag [32]; 
-    
+        
     int i;
     initial begin
         for(i = 0; i < 32; i++) begin
@@ -53,6 +60,13 @@ module ARF(
                     set_tag[a] = 0;
             end
         end
+         
+        //on posedge clk, set all logical_dest registers as invalid to avoid accidentally using these values
+        for(a = 0; a < 4; a = a + 1) begin
+            always @(posedge clk) begin
+                valid[logical_dest[a]] = 0;
+            end
+        end
     endgenerate
     
     genvar b;
@@ -61,7 +75,7 @@ module ARF(
             always_comb begin
                 if(set_tag[logical_dest[b]] & !no_available)
                     intermediate_tag[logical_dest[b]] = physical_dest[b];
-                 else
+                else
                     intermediate_tag[logical_dest[b]] = tag[logical_dest[b]];
             end
         end
@@ -69,6 +83,7 @@ module ARF(
         //VERY IMPORTANT TO SEE: TO ENSURE THAT TAG CAN BE READ ON NEXT CYCLE THIS IS WRITTEN ON NEGEDGE
         //THIS MIGHT BE TOO SLOW SO LOOK FOR ALTERNATIVE SOLUTIONS
         //See github issue for potential solution
+        //UPDATE: should be implemented, haven't tested too thoroughly though
         for(b = 0; b < 32; b = b + 1) begin
             always @(posedge clk) begin
                 if(!no_available) begin
--- project_1.srcs/sources_1/new/rename_top.sv
+++ project_1.srcs/sources_1/new/rename_top.sv
@@ -1,26 +1,56 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/28/2023 07:01:19 PM
-// Design Name: 
-// Module Name: rename_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module rename_top(
+        input clk,
+        input [4:0] dest [4],
+        input [4:0] r1 [4],
+        input [4:0] r2 [4],
+        input [3:0] dest_valid,
+        input stall_external,
+        output [6:0] physical_dest [4],
+        output [3:0] physical_dest_valid,
+        output [31:0] r1_out [4],
+        output [31:0] r2_out [4],
+        output stall_internal
+    );
+    
+    wire [6:0] allocated [4];
+    wire no_available;
 
+    
+    ARF reg_file(
+        .logical_dest (dest),
+        .logical_dest_valid (dest_valid),
+        .clk (clk),
+        .physical_dest (allocated),
+        .no_available (no_available),       
+        .r1 (),          
+        .r2 (),           
+        .r1_out (),    
+        .r2_out (),
+        .r1_ready (),
+        .r2_ready ()
+    );
+    
+    
+    ROB buffer(
+        .num_writes (),
+        .clk (),
+        .if_reg (), 
+        .dest (), 
+        .r1 (), 
+        .r2 (), 
+        .r1_valid (), 
+        .r2_valid (),
+        .r1_out (), 
+        .r2_out (),
+        .no_available (no_available),
+        .allocated (allocated),
+        .committed (),
+        .committed_dest (),
+        .committed_source (),
+        .num_commits (),
+        .num_available_out ()//just num available, used for ARF
     );
 endmodule
