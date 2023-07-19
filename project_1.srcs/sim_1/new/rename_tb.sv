95931dd18e4a868a6bd9cb6126a3763b8945967f
vbogdev <Vbogdev2@gmail.com>
2023-07-19 03:15:12 -0700

    tb

---
+++ project_1.srcs/sim_1/new/rename_tb.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+
+module rename_tb(
+
+    );
+    
+    bit clk;
+    bit [2:0] num_writes;
+    bit [4:0] dest[4];
+    bit [4:0] r1 [4];
+    bit [4:0] r2 [4];
+    bit [3:0] dest_valid;
+    bit stall_external;
+    
+    wire [6:0] physical_dest [4];
+    wire [3:0] physical_dest_valid;
+    wire [3:0] r1_ready, r2_ready;
+    wire [6:0] r1_source [4];
+    wire [6:0] r2_source [4];
+    wire stall_internal;
+    
+    rename_top DUT(
+        .clk, 
+        .num_writes, 
+        .dest, 
+        .r1, 
+        .r2, 
+        .dest_valid, 
+        .stall_external, 
+        .physical_dest, 
+        .physical_dest_valid, 
+        .r1_ready,
+        .r2_ready,
+        .r1_source,
+        .r2_source,
+        .stall_internal
+    );
+    
+    initial begin
+    
+    end
+    
+    always begin
+        clk = 0;
+        #10;
+        clk = 1;
+        #10;
+    end
+endmodule
